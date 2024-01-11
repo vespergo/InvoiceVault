d985413d2cfdea0a0b73ed215a10d3d67cc58c58
Josh O'Bryan <vespergo@gmail.com>
2024-01-10 20:57:44 -0600

    Updated logo. favicon. swapped out homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,29 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
-import { lusitana } from './ui/fonts';
-import Image from 'next/image';
+
+
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
-          <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <AcmeLogo />
+            <h1 className="text-4xl font-bold text-gray-800 mb-6">
+                Welcome to InvoiceVault
+            </h1>
+            <p className="text-lg text-gray-700 text-center max-w-2xl mb-6">
+                Your one-stop solution for managing and storing customer invoices.
+                Easy to use, secure, and efficient, InvoiceVault helps you keep track of
+                all your billing needs in one place.
+            </p>
+           
+            <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className=" rounded-lg bg-blue-500 px-10 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> 
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshots of the dashboard project showing mobile version"
-          />
-        </div>
-      </div>
-    </main>
+          
   );
 }
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,14 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { InboxStackIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image src='/InvoiceVault.png' alt="InvoiceVault Logo" width={150} height={150} />
+      
     </div>
   );
 }

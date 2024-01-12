a58686a79a1050ee57b0a8212ed63cd6835ee409
Tselot Tesfaye <tselot63@gmail.com>
2024-01-12 13:56:19 -0600

    finished the react task

---
+++ app/ui/customers/create-form.tsx
@@ -0,0 +1,106 @@
+'use client';
+import { CustomerField } from '@/app/lib/definitions';
+import Link from 'next/link';
+import { Button } from '@/app/ui/button';
+import { useFormState } from 'react-dom';
+import { createCustomer } from '@/app/lib/actions';
+
+export default function CustomerForm({ customers }: { customers: CustomerField[] }) {
+    const initialState = { message: null, errors: {} };
+    const [state, dispatch] = useFormState(createCustomer, initialState);
+    console.log(state);
+    return (
+        <form action={dispatch}>
+            <div className="rounded-md bg-gray-50 p-4 md:p-6">
+                {/* Customer Name */}
+                <div className="mb-4">
+                    <label htmlFor="name" className="mb-2 block text-sm font-medium">
+                        Customer Name
+                    </label>
+                    <div className="relative mt-2 rounded-md">
+                        <div className="relative">
+                            <input
+                                id="name"
+                                name="name"
+                                type="text"
+                                placeholder="Enter Customer Name"
+                                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                aria-describedby="name-error"
+                            />
+                        </div>
+                    </div>
+                    <div id="name-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.name &&
+                            state.errors.name.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
+                </div>
+                {/* Customer Email */}
+                <div className="mb-4">
+                    <label htmlFor="email" className="mb-2 block text-sm font-medium">
+                        Customer Email
+                    </label>
+                    <div className="relative mt-2 rounded-md">
+                        <div className="relative">
+                            <input
+                                id="email"
+                                name="email"
+                                type="text"
+                                placeholder="Enter Customer Email"
+                                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                aria-describedby="email-error"
+                            />
+                        </div>
+                    </div>
+                    <div id="email-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.email &&
+                            state.errors.email.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
+                </div>
+                {/* Customer Name */}
+                <div className="mb-4">
+                    <label htmlFor="image_url" className="mb-2 block text-sm font-medium">
+                        Customer Image
+                    </label>
+                    <div className="relative mt-2 rounded-md">
+                        <div className="relative">
+                            <input
+                                id="image_url"
+                                name="image_url"
+                                type="file"
+                                accept=".png"                                 placeholder="Upload Image"
+                                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2"
+                                aria-describedby="image-error"
+                            />
+                        </div>
+                    </div>
+                    <div id="image-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.image_url &&
+                            state.errors.image_url.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
+                </div>
+
+            </div>
+            <div className="mt-6 flex justify-end gap-4">
+                <Link
+                    href="/dashboard/customers"
+                    className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+                >
+                    Cancel
+                </Link>
+                <Button type="submit">Create Customer</Button>
+            </div>
+        </form>
+    );
+}
